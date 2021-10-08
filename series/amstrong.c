333c5458be5cf2a58c5ea0374f3500b193ef0d75
MASHOD0 <mashhood2002@gmail.com>
2021-10-08 08:20:00 +0530

    added amstrong.c

---
+++ series/amstrong.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+int main(void){
+    for(int i = 1 ; i <= 100; i++){
+        int amstrong, sum, arr[3];
+        int num = i;
+        while(n > 0){
+            int j = 0;
+            r = num % 10;
+            arr[j] = r;
+            num = num / 10;
+        }
+        for (int k = 0 ; k < 2; k++){
+            sum += pow(arr[k], 3)
+        }
+        if(sum == i)
+        amstrong++;
+    }
+    return 0;
+}
