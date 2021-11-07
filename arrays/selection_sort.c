328a6da606e60d55308e1f59bc8e363e61a1f8ac
MASHOD0 <mashhood2002@gmail.com>
2021-11-07 21:19:17 +0530

    addedd selection sort"

--- arrays/selection_sort.c
+++ arrays/selection_sort.c
@@ -2,5 +2,18 @@
 #include <stdio.h>
 
 int main(void){
-    
+    //selection sort
+    int array[5] = {4, 3, 2, 1, 0};
+    int i, j, min_index, temp;
+    for(i = 0; i < 5; i++){
+        min_index = i;
+        for(j = i + 1; j < 5; j++){
+            if(array[j] < array[min_index]){
+                min_index = j;
+            }
+        }
+        temp = array[i];
+        array[i] = array[min_index];
+        array[min_index] = temp;
+    }
 }
