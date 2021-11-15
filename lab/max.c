c33782602372a75108ff7f3078c7a7c2456f3c3c
MASHOD0 <mashhood2002@gmail.com>
2021-11-15 15:42:35 +0530

    max.c

---
+++ lab/max.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(void){
+    int in[8];
+    printf("enter the 8 bit binary value:");
+    for(int i=0;i<8;i++){
+        scanf("%d",&in[i]);
+    }
+    for(int i=0;i<8;i++){
+        if(in[i]==1){
+            in[i]=0;
+        }
+        else if(in[i]==0){
+            in[i]=1;
+        }
+    }
+    for(int i=0;i<8;i++){
+        printf("%d",in[i]);
+    }
+}
