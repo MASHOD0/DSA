eb369d65a3fc9ae0a6a69269400435d27edf4904
MASHOD0 <mashhood2002@gmail.com>
2021-12-12 14:13:44 +0530

    cie-1 change

--- series/amstrong.c
+++ series/amstrong.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 #include<math.h>
 int main(void){
+    int amstrong = 0;
     for(int i = 1 ; i <= 100; i++){
-        int amstrong, sum, arr[3];
+        int r, n, sum, arr[3];
         int num = i;
-        while(n > 0){
+        while(n < 0){
             int j = 0;
             r = num % 10;
             arr[j] = r;
             num = num / 10;
+            printf("%d \n", r);
         }
         for (int k = 0 ; k < 2; k++){
-            sum += pow(arr[k], 3)
+            sum += pow(arr[k], 3);
         }
         if(sum == i)
         amstrong++;
     }
+    printf(" the number of amstrong numbers: %d", amstrong);
     return 0;
 }
