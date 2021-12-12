e1945637d601b18217b9f3f0a58ea156da753bd4
MASHOD0 <mashhood2002@gmail.com>
2021-12-12 14:09:16 +0530

    init

---
+++ stack/evaluation_postfix.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+
+int stack[20], top=-1;
+void push(int x)
+{
+stack[++top]=x;
+}
+int pop()
+{
+return(stack[top--]);
+}
+void main()
+{
+char s[20];
+int x, i, y;
+//clrscr();
+printf("\n evaluation of postfix expression \n");
+printf("\n enter a postfix expression ");
+gets(s);
+for(i=0;i<strlen(s);i++)
+{
+if(isdigit(s[i]))
+push(s[i]-'0');
+else
+{
+y=pop();
+x=pop();
+switch(s[i])
+{
+case '+':push(x+y); break;
+case '-':push(x-y); break;
+case '*':push(x*y); break;
+case '/':push(x/y); break;
+case '^':
+case '$':push(pow(x,y)); break;
+}
+}
+}
+printf("\n result is %d ",pop());
+//getch();
+}
